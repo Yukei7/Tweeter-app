be6f5c8e95b7d2c347e2ddce403627df8b130ee1
yukei7 <yukei0509@gmail.com>
2022-06-23 16:22:47 -0700

    Add Friendship model

---
+++ friendships/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Friendship(models.Model):
+    # Related name: following_friendship_set
+    # => User.objects.filter(following_friendship_set=user)
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="following_friendship_set",
+    )
+    # Related name: follower_friendship_set
+    # => User.objects.filter(follower_friendship_set=user)
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="follower_friendship_set",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = (
+            ('from_user_id', 'created_at'),
+            ('to_user_id', 'created_at'),
+        )
+        unique_together = (('from_user_id', 'to_user_id'), )
+
+    def __str__(self):
+        return f"{self.from_user.id} followed {self.to_user.id}"
