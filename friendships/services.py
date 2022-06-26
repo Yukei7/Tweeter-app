99b8059cd48174d1a74cf3026d23d9d9e63fb0f2
yukei7 <yukei0509@gmail.com>
2022-06-26 03:25:03 -0700

    Fix services bug: return user from friendship services.

--- friendships/services.py
+++ friendships/services.py
@@ -7,4 +7,4 @@ class FriendshipService:
     @classmethod
     def get_followers(cls, user):
         friendships = Friendship.objects.filter(to_user=user).prefetch_related('from_user')
-        return [friendship for friendship in friendships]
+        return [friendship.from_user for friendship in friendships]
--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -6,8 +6,10 @@ class NewsFeedService(object):
     @classmethod
     def fanout_to_followers(cls, tweet):
         followers = FriendshipService.get_followers(tweet.user)
-        newsfeeds = [NewsFeed(user=follower, tweet=tweet) for follower in followers]
+        newsfeeds = [
+            NewsFeed(user=follower, tweet=tweet)
+            for follower in followers
+        ]
         # add self
         newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
         NewsFeed.objects.bulk_create(newsfeeds)
-        
