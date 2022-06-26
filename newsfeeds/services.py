68cb731a4902096f00f36188e087ce1ed8a17a14
yukei7 <yukei0509@gmail.com>
2022-06-26 02:58:48 -0700

    Add services: fanout tweets

---
+++ newsfeeds/services.py
@@ -0,0 +1,13 @@
+from friendships.services import FriendshipService
+from newsfeeds.models import NewsFeed
+
+
+class NewsFeedService(object):
+    @classmethod
+    def fanout_to_followers(cls, tweet):
+        followers = FriendshipService.get_followers(tweet.user)
+        newsfeeds = [NewsFeed(user=follower, tweet=tweet) for follower in followers]
+        # add self
+        newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
+        NewsFeed.objects.bulk_create(newsfeeds)
+        
